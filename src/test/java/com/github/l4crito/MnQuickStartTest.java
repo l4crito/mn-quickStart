7b454132f551aac28c5315b4aa78fba0fd5488a4
l4crito <l4crito>
2021-11-10 22:05:04 +0000

    Initial commit

---
+++ src/test/java/com/github/l4crito/MnQuickStartTest.java
@@ -0,0 +1,21 @@
+package com.github.l4crito;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MnQuickStartTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
